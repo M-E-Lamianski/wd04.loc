e01dc85d8b4a9b29c696207296bfa8a7892b7229
Maksim Lamianski <iksnaimal@gmail.com>
2022-04-04 14:46:29 +0300

    Add admin

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,15 @@ Route::get('fullName', [\App\Http\Controllers\MyController::class, 'myPage']);
 //Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('admin', [\App\Http\Controllers\Admin\AdminController::class, 'dashboard']);
+Route::get('admin/articles', [\App\Http\Controllers\Admin\ArticleController::class, 'index']);
+Route::get('admin/articles/create', [\App\Http\Controllers\Admin\ArticleController::class, 'create']);
+Route::post('admin/articles/store', [\App\Http\Controllers\Admin\ArticleController::class, 'store'])
+    ->name('admin.store_article');
+Route::get('admin/articles/{id}/edit', [\App\Http\Controllers\Admin\ArticleController::class, 'edit'])
+    ->name('admin.edit_article');
+Route::put('admin/articles/{id}/update', [\App\Http\Controllers\Admin\ArticleController::class, 'update'])
+    ->name('admin.update_article');
+Route::delete('admin/articles/{id}/delete', [\App\Http\Controllers\Admin\ArticleController::class, 'delete'])
+    ->name('admin.delete_article');
