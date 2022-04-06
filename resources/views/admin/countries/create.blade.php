bb14b7004293070857af776eed054f18b3d3ddae
Maksim Lamianski <iksnaimal@gmail.com>
2022-04-06 18:52:24 +0300

    Add Request and CountriesController

--- resources/views/admin/countries/create.blade.php
+++ resources/views/admin/countries/create.blade.php
@@ -4,19 +4,18 @@
     <div class="grid-form">
         <div class="grid-form1">
             <h3 id="forms-example" class="">Добавление записи</h3>
-            <form action="{{ route('countries.update', ['id' => $country->id ]) }}" method="POST" enctype="multipart/form-data">
-                @method('PUT')
+            <form action="{{ route('admin.countries.store') }}" method="POST" enctype="multipart/form-data">
                 @csrf
                 <div class="form-group">
                     <label>Название страны</label>
-                    <input type="text" name="name" class="form-control" value="{{$country->name}}">
+                    <input type="text" name="name" class="form-control">
                 </div>
                 <div class="form-group">
                     <label>Код страны</label>
-                    <input type="text" name="code" class="form-control" value="{{$country->code}}">
+                    <input type="text" name="code" class="form-control">
                 </div>
                 <button type="submit" class="btn btn-default">Submit</button>
-                <a href="{{ route('countries.index') }}" class="btn btn-default">Cancel</a>
+                <a href="{{ route('admin.countries.index') }}" class="btn btn-default">Cancel</a>
             </form>
         </div>
     </div>
